11a82171735697ac5e173fc624173b8158c013e0
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-11-16 22:35:59 +0300

    Feat(root): add memory storage

--- src/apps/root/__init__.py
+++ src/apps/root/__init__.py
@@ -1,5 +1,7 @@
 from aiogram import Dispatcher, Bot
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from modules.config.env_config import TOKEN
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
