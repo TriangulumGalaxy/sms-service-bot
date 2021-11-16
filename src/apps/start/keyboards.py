94f79001e7245a541a808c9acc0cc5200523f011
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-11-16 22:37:29 +0300

    Feat(registration): api key check keyboard

--- src/apps/start/keyboards.py
+++ src/apps/start/keyboards.py
@@ -1,4 +1,5 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types.inline_keyboard import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 langs = (
@@ -25,3 +26,7 @@ langs = (
 lang_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=True)
 for i in langs:
     lang_keyboard.insert(KeyboardButton(i))
+
+boolean_keyboard = InlineKeyboardMarkup(row_width=2)
+boolean_keyboard.insert(InlineKeyboardButton("Да", callback_data="answer_y"))
+boolean_keyboard.insert(InlineKeyboardButton("Нет", callback_data="answer_n"))
