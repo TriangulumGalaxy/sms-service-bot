2dd8288c17678f9533613d8a6ba7c5db4b5d6ab6
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-11-18 22:35:26 +0300

    Feat(db): add db import

--- src/apps/root/__init__.py
+++ src/apps/root/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Dispatcher, Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from modules.config.env_config import TOKEN
+from modules.db.database import db
 
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
