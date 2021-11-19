c9880216e03239e3ae2864ecc5aa5b4282b78100
TriangulumGalaxy <59769865+TriangulumGalaxy@users.noreply.github.com>
2021-11-19 21:31:10 +0300

    feat: add function connect to db

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from aiogram import executor
 from apps import dp, db
 # from apps import _filters
 
+
 async def flush_all():
     print('Очистка базы...')
     await db.gino.drop_all()
@@ -10,9 +11,8 @@ async def flush_all():
     await db.gino.create_all()
     print('Готово')
 
-async def on_startup(dp):
-    print("bot has started")
-    
+
+async def connect_db():
     from modules.db import database
 
     print('Подключение к базе данных...')
@@ -20,15 +20,21 @@ async def on_startup(dp):
     print('Подключение установлено')
     await flush_all()
 
+
+async def on_startup(dp):
+    print("bot has started")
+    # await connect_db()
+
 def bind_filters(dp, *args):
     """
     `args`: фильтры
     `dp`: Dispatcher
     """
-    
+
     filters = args[0]
     for fltr in filters:
         dp.bind_filter(fltr)
+
 
 if __name__ == '__main__':
     # bind_filters(dp, _filters)
