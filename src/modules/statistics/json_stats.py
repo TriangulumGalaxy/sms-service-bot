4c50dd04583532dcd0d8a3da1707c6208a3a2e4e
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-11-21 16:13:09 +0300

    Fix(stats): fix single stats params update

--- src/modules/statistics/json_stats.py
+++ src/modules/statistics/json_stats.py
@@ -2,13 +2,14 @@ import json
 
 
 async def update_param(param: str):
-    with open('stats.json', '+') as f:
+    with open('modules/statistics/stats.json', 'r') as f:
         json_data = json.load(f)
         json_data[param] += 1
+    with open('modules/statistics/stats.json', 'w') as f:
         f.write(json.dumps(json_data))
 
 
 async def get_param(param: str) -> int:
-    with open('stats.json', '+') as f:
+    with open('modules/statistics/stats.json', 'r') as f:
         json_data = json.load(f)
         return json_data[param]
