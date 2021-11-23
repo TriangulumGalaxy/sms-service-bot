a7ce394cc356c03f3356a522a63f7e0d73d2b57a
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-11-23 17:33:29 +0300

    Feat(stats): add absolute path for stats.json with os module

--- src/modules/statistics/json_stats.py
+++ src/modules/statistics/json_stats.py
@@ -1,15 +1,19 @@
 import json
+import os
 
 
 async def update_param(param: str):
-    with open('modules/statistics/stats.json', 'r') as f:
+    with open(os.path.abspath('src/modules/statistics/stats.json'), 'r', encoding='utf-8') as f:
         json_data = json.load(f)
         json_data[param] += 1
-    with open('modules/statistics/stats.json', 'w') as f:
+    with open(os.path.abspath('src/modules/statistics/stats.json'), 'w', encoding='utf-8') as f:
         f.write(json.dumps(json_data))
 
 
-async def get_param(param: str) -> int:
-    with open('modules/statistics/stats.json', 'r') as f:
+async def get_params():
+    with open(os.path.abspath('src/modules/statistics/stats.json'), 'r', encoding='utf-8') as f:
         json_data = json.load(f)
-        return json_data[param]
+        answer = 'Общая статистика\n\n'
+        for param, num in json_data.items():
+            answer += f'{param}: {num}\n'
+        return answer
