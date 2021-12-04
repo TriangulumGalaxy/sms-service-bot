63565b953658b2cdfd355ebff1fdee67ef97f7f3
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-12-04 14:43:09 +0300

    Fix(statistics): fix json stats

--- src/modules/statistics/json_stats.py
+++ src/modules/statistics/json_stats.py
@@ -4,15 +4,15 @@ import os
 
 async def update_param(param: str):
     with open(os.path.abspath('src/modules/statistics/stats.json'), 'r', encoding='utf-8') as f:
-        json_data = json.load(f)
+        json_data = json.loads(f.read())
         json_data[param] += 1
     with open(os.path.abspath('src/modules/statistics/stats.json'), 'w', encoding='utf-8') as f:
-        f.write(json.dumps(json_data))
+        f.write(json.dumps(json_data, ensure_ascii=False))
 
 
 async def get_params():
     with open(os.path.abspath('src/modules/statistics/stats.json'), 'r', encoding='utf-8') as f:
-        json_data = json.load(f)
+        json_data = json.loads(f.read())
         answer = 'Общая статистика\n\n'
         for param, num in json_data.items():
             answer += f'{param}: {num}\n'
